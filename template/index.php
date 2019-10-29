78a41d3ba4303c806e3b2c8c47e0ecd6372b0a54
Gwyneth <gwyneth_schmale@hotmail.nl>
2019-10-29 12:55:24 +0100

    css ff aan index gelinkt, ben bezig met de andere links ook fixen

--- template/index.php
+++ template/index.php
@@ -1,1 +1,41 @@
-<?php include 'php/nav.php'; ?>
+
+<!--made by Gwyneth-->
+<?php include 'php/nav.php';?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style3.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <title>Template</title><!--Change to your name!-->
+    <!--ignore this, it's nothing-->
+    <audio src="assets/audio/cave1.ogg" type="audio/ogg" id="cave1"></audio>
+    <audio src="assets/audio/cave2.ogg" type="audio/ogg" id="cave2"></audio>
+    <audio src="assets/audio/cave3.ogg" type="audio/ogg" id="cave3"></audio>
+    <audio src="assets/audio/cave4.ogg" type="audio/ogg" id="cave4"></audio>
+    <audio src="assets/audio/cave5.ogg" type="audio/ogg" id="cave5"></audio>
+    <audio src="assets/audio/cave6.ogg" type="audio/ogg" id="cave6"></audio>
+    <audio src="assets/audio/cave7.ogg" type="audio/ogg" id="cave7"></audio>
+    <audio src="assets/audio/cave8.ogg" type="audio/ogg" id="cave8"></audio>
+    <audio src="assets/audio/cave10.ogg" type="audio/ogg" id="cave10"></audio>
+    <audio src="assets/audio/cave11.ogg" type="audio/ogg" id="cave11"></audio>
+    <audio src="assets/audio/cave12.ogg" type="audio/ogg" id="cave12"></audio>
+    <audio src="assets/audio/cave13.ogg" type="audio/ogg" id="cave13"></audio>
+  </head>
+  <body>
+
+    <div id="idBody"class="wrapper">
+
+    </div>
+
+
+
+
+    <!--scripts-->
+    <script src="script/nav.js"></script>
+    <script src="script/init.js"></script>
+    <script src="script/3am.js"></script>
+  </body>
+</html>
