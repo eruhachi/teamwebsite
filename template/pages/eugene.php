8e14ea269bfe0bbeb72a1c5ba3ba3f82d81323ff
Eugene van Dolderen <eugenevandolderen@hotmail.com>
2019-10-29 14:41:17 +0100

    Eigen pagina

--- template/pages/eugene.php
+++ template/pages/eugene.php
@@ -1,4 +1,3 @@
-<!--made by Gwyneth-->
 <?php include '../php/nav.php';?>
 
 
@@ -8,8 +7,7 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="../css/style3.css">
     <link rel="stylesheet" href="../css/nav.css">
-    <title>your name</title><!--Change to your name!-->
-    <!--ignore this, it's nothing-->
+    <title>Eugene van Dolderen</title>
     <audio src="../assets/audio/cave1.ogg" type="audio/ogg" id="cave1"></audio>
     <audio src="../assets/audio/cave2.ogg" type="audio/ogg" id="cave2"></audio>
     <audio src="../assets/audio/cave3.ogg" type="audio/ogg" id="cave3"></audio>
@@ -29,34 +27,54 @@
 
        <div class="grid">
    <!--content 1-->
-    <!--replace the # with the name of your image-->
-    <div class="img1"> <img src="../assets/IMG/#" alt=""> </div>
+    <div class="img1"> <img src="../assets/IMG/Eugene.jpg" alt=""> </div>
 
     <div class="text-line1"></div>
     <div class="text-box1">
       <h1>Wie ben ik</h1><br>
-    <p><!--put your text here--></p>
+    <p>Mijn naam is Eugene van Dolderen, ik ben op dit moment 16 jaar oud.
+      Ik ben een best gemotiveerd persoon. Dat zou u ook best kunnen zien
+      hoe snel ik dingen maak.  Ik denk gewoon zo van: ”Je kan gaan gamen en
+      gewoon random dingen doen als je dingen af hebt.” Ik zou bijvoorbeeld
+      er echt niet tegen kunnen als ik dingen niet af zou hebben.  Ik ga net zolang
+      door totdat het af is en werk compleet goed gemaakt is.  Ik ben ook heel erg
+      pietje precies, dus ik ben pas klaar totdat ik tevreden ben!
+      Verder is mijn leven wat gecompliceerder dan de gewone mens, want ik heb namelijk een handicap: <a href="https://www.hersenletsel-uitleg.nl/oorzaken-ziektebeelden/cerebrale-parese-cp">Cere Brale Parese</a> <br>
+      Verder woon ik gewoon nog thuis met mijn ouders en zus en een hondje!
+      </p>
     </div>
-    <div class="deco1"><img src="../assets/IMG/deco1.gif" alt=""> </div>
-    <div class="deco3"><img src="../assets/IMG/deco3.png" alt=""> </div>
+    <div class="deco1"><img src="../assets/IMG/Yasuo.jpg" alt=""> </div>
+    <div class="deco3"><img src="../assets/IMG/Eugene1.jpg" alt=""> </div>
 
 <!--content 2-->
     <div class="text-box2">
       <h1>Mijn sterke..</h1><br>
-    <p><!--put your text here--></p> </div>
+    <p>Oehh dan vraag je me wat, want ik vraag dat vaak aan mezelf maar het is redelijk moeilijk om naar jezelf te kijken voor wat je aan het doen bent.
+Maarehh ik kan wel op wat komen hoor!
+Ik ben best snel in nieuwe dingen leren en zoals ik al hierboven heb verteld is dat ik heel snel werk en dat ik heel gemotiveerd ben!
+</p> </div>
     <div class="text-line2"></div>
     <div class="text-box2-1">
     <h1>en minder sterke punten</h1><br>
-    <p><!--put your text here--></p> </div>
-    <div class="deco2"> <img src="../assets/IMG/deco2.png" alt="nintendo"></div>
+    <p>Nu maar even kijken naar m’n slechtere punten:
+Mijn communicatie is slecht, omdat ik gewend ben om gewoon voor mezelf te werken zonder enig gepraat ofzoiets. Dus daarom heb ik 9 van de 10 keer oordopjes in, om gewoon voor mezelf te gaan werken.
+Het presenteren gaat bij mij ook niet zoals ik het wil, want ik wil altijd veels te veel laten zien.
+Dus dat zijn twee eigenschappen waar ik aan kan werken.
+</p> </div>
+    <div class="deco2"> <img src="../assets/IMG/jinx.jpg" alt="nintendo"></div>
 
 <!--content 3-->
     <div class="text-box3">
       <h1>Projecten en opdrachten waar ik aan heb gewerkt</h1><br>
-    <p><!--put your text here--></p></div>
-    <div class="deco4"><img src="../assets/IMG/deco1.gif" alt="bow"></div><div class="text-line3"></div><div class="deco5"><img src="../assets/IMG/deco1.gif" alt="bow"></div>
+    <p>Op school hebben we gegarandeerd projecturen, dus dan word aan jou gevraagd of je bijvoorbeeld naar een bedrijf wilt en checken of het goede hackbeveiliging heeft.
+Of je gaat zelf naar de docent die over de projecturen gaat, dan zal je zelf iets moeten weten waar ze je hulp nodig hebben om iets te controleren, of iets heel anders gaan maken.
+</p></div>
+    <div class="deco4"><img src="../assets/IMG/bruinzwaard.jpg" alt="bow"></div><div class="text-line3"></div><div class="deco5"><img src="../assets/IMG/zwartzwaard.jpg" alt="bow"></div>
     <div class="text-box3-1">
-    <p><!--put your text here--></p></div>
+    <p>Bijvoorbeeld mijn vader werkt bij Defensie, misschien hebben hun een website nodig. Als ik dan word gevraagd, dan moet ik naar de docent gaan, dan gaan vragen of al dat kan.
+    Ik ben zoals ik al 2x heb verteld heel gemotiveerd. Dus wat mij betreft wil ik alles kunnen wat met programmeren te maken heeft.
+    Dus als het om web development/maintaince gaat dan wel.
+    Ookal gaat het om een game </p></div>
 
 
 
@@ -64,8 +82,8 @@
   <footer>
   <div class="nested-footer">
   <p>© eruhachi</p>
-  <div class="icon1"><a href="https://www.instagram.com/eruhachi/" target="_blank"><img src="../assets/IMG/instagram.png" alt="instagram"></a></div>
-  <div class="icon2"><a href="https://www.youtube.com/user/BenIkJouOma" target="_blank"><img src="../assets/IMG/youtube.png" alt="youtube"></a></div>
+  <div class="icon1"><a href="https://www.instagram.com/eugenevandolderen/" target="_blank"><img src="../assets/IMG/instagram.png" alt="instagram"></a></div>
+  <div class="icon2"><a href="https://www.youtube.com/channel/UCo7gtii_pXiaMGzZayxBQtg" target="_blank"><img src="../assets/IMG/youtube.png" alt="youtube"></a></div>
 
 
 
