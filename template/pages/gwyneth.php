c0ba04dc95c7560c4a9a0665df2e163ca2ec5003
Gwyneth <gwyneth_schmale@hotmail.nl>
2019-10-29 13:05:53 +0100

    again just linking shit aaa

--- template/pages/gwyneth.php
+++ template/pages/gwyneth.php
@@ -1,14 +1,14 @@
 <!--made by Gwyneth-->
-<?php include 'php/nav.php';?>
+<?php include '../php/nav.php';?>
 
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="css/style3.css">
-    <link rel="stylesheet" href="css/nav.css">
-    <title>Template</title><!--Change to your name!-->
+    <link rel="stylesheet" href="../css/style3.css">
+    <link rel="stylesheet" href="../css/nav.css">
+    <title>Gwyneth</title><!--Change to your name!-->
     <!--ignore this, it's nothing-->
     <audio src="assets/audio/cave1.ogg" type="audio/ogg" id="cave1"></audio>
     <audio src="assets/audio/cave2.ogg" type="audio/ogg" id="cave2"></audio>
@@ -29,14 +29,14 @@
 
        <div class="grid">
    <!--content 1-->
-    <div class="img1"> <img src="assets/IMG/gwyn2.jpg" alt=""> </div>
+    <div class="img1"> <img src="../assets/IMG/gwyn2.jpg" alt=""> </div>
     <div class="text-line1"></div>
     <div class="text-box1">
       <h1>Wie ben ik</h1><br>
     <p>Mijn naam is Gwyneth, ik ben 17. Ik game sinds mijn 8ste en ik hou erg van muziek. Ik speel piano, gitaar en viool. Ik ben geobsedeerd met nintendo, computers en vaporwave. Ik vind het ook leuk om te tekenen, te photoshoppen en video's te editen. Ik hou van talen en van anime, hierdoor ben ik Japans gaan leren.</p>
     </div>
-    <div class="deco1"><img src="assets/IMG/deco1.gif" alt=""> </div>
-    <div class="deco3"><img src="assets/IMG/deco3.png" alt=""> </div>
+    <div class="deco1"><img src="../assets/IMG/deco1.gif" alt=""> </div>
+    <div class="deco3"><img src="../assets/IMG/deco3.png" alt=""> </div>
 
 <!--content 2-->
     <div class="text-box2">
@@ -46,13 +46,13 @@
     <div class="text-box2-1">
     <h1>en minder sterke punten</h1><br>
     <p>Ik wil iedereen helpen als iemand er om vraagt. Ik ben alleen niet zo goed in uitleggen en ik kom soms niet zo goed uit mijn woorden.</p> </div>
-    <div class="deco2"> <img src="assets/IMG/deco2.png" alt="nintendo"></div>
+    <div class="deco2"> <img src="../assets/IMG/deco2.png" alt="nintendo"></div>
 
 <!--content 3-->
     <div class="text-box3">
       <h1>Projecten en opdrachten waar ik aan heb gewerkt</h1><br>
     <p>Ik heb voor een opdracht op mijn vorige school een website gemaakt. Het was de bedoeling dat je deed alsof je een sportvereniging was. Dit is een van de eerste keren dat ik met HTML en CSS heb gewerkt. Het was niet een hele goede website maar ik heb er heel veel van geleerd.</p></div>
-    <div class="deco4"><img src="assets/IMG/deco1.gif" alt="bow"></div><div class="text-line3"></div><div class="deco5"><img src="assets/IMG/deco1.gif" alt="bow"></div>
+    <div class="deco4"><img src="../assets/IMG/deco1.gif" alt="bow"></div><div class="text-line3"></div><div class="deco5"><img src="../assets/IMG/deco1.gif" alt="bow"></div>
     <div class="text-box3-1">
     <p>Ik moest daarna een website over een game maken en door alle fouten die ik in de vorige opdracht heb gemaakt, is deze website heel veel beter geworden.</p><p>Ik had door deze opdrachten al een redelijke basis van HTML en CSS, ik ben daarna verder gegaan met JS en PHP. Ik heb ook ooit in unity pong geprobeerd te programmeren in C#.</p></div>
 
@@ -62,8 +62,8 @@
   <footer>
   <div class="nested-footer">
   <p>© eruhachi</p>
-  <div class="icon1"><a href="https://www.instagram.com/eruhachi/" target="_blank"><img src="assets/IMG/instagram.png" alt="instagram"></a></div>
-  <div class="icon2"><a href="https://www.youtube.com/user/BenIkJouOma" target="_blank"><img src="assets/IMG/youtube.png" alt="youtube"></a></div>
+  <div class="icon1"><a href="https://www.instagram.com/eruhachi/" target="_blank"><img src="../assets/IMG/instagram.png" alt="instagram"></a></div>
+  <div class="icon2"><a href="https://www.youtube.com/user/BenIkJouOma" target="_blank"><img src="../assets/IMG/youtube.png" alt="youtube"></a></div>
 
 
 
--- template/php/nav.php
+++ template/php/nav.php
@@ -6,13 +6,13 @@
    </div>
   <ul class="nav-links">
     <li class="bold"><a href="../index.php">Het team</a></li>
-    <li><a href="../pages/gwyneth.php">Gwyneth</a></li>
-    <li><a href="../pages/jeroen.php">Jeroen</a></li>
-    <li><a href="../pages/eelke.php">Eelke</a></li>
-    <li><a href="../pages/jonean.php">Jonean</a></li>
-    <li><a href="../pages/rowan.php">Rowan</a></li>
-    <li><a href="../pages/eugene.php">Eugene</a></li>
-    <li><a href="../pages/rick.php">Rick</a></li>
+    <li><a href="pages/gwyneth.php">Gwyneth</a></li>
+    <li><a href="pages/jeroen.php">Jeroen</a></li>
+    <li><a href="pages/eelke.php">Eelke</a></li>
+    <li><a href="pages/jonean.php">Jonean</a></li>
+    <li><a href="pages/rowan.php">Rowan</a></li>
+    <li><a href="pages/eugene.php">Eugene</a></li>
+    <li><a href="pages/rick.php">Rick</a></li>
   </ul>
   <!--responsive nav-->
     <div class="stripes-menu">
