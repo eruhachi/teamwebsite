29e21406a63508776770aec25ed5d1cd1bdbffed
HSUNITY <rick.wiersma@knid.nl>
2019-10-31 11:44:13 +0100

    Update rick.php
    "Wie ben ik" verhaal aangepast

--- template/pages/rick.php
+++ template/pages/rick.php
@@ -1,73 +1,74 @@
 <!--made by Gwyneth-->
-<?php include '../php/nav.php';?>
+<?php include '../php/nav.php'; ?>
 
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="../css/style3.css">
-    <link rel="stylesheet" href="../css/rick.css">
-    <link rel="stylesheet" href="../css/nav.css">
-    <title>Rick Wiersma</title><!--Change to your name!-->
-  </head>
-  <body>
 
-    <div id="idBody"class="wrapper">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="../css/style3.css">
+  <link rel="stylesheet" href="../css/rick.css">
+  <link rel="stylesheet" href="../css/nav.css">
+  <title>Rick Wiersma</title>
+</head>
 
-       <div class="grid">
-   <!--content 1-->
-    <!--replace the # with the name of your image-->
-    <div class="img1"> <img src="../assets/IMG/portretrick.jpeg" alt=""> </div>
+<body>
 
-    <div class="text-line1"></div>
-    <div class="text-box1">
-      <h1>Wie ben ik</h1><br>
-    <p>Mijn naam is Rick Wiersma, ik ben 17 jaar en woon in Metslawier.<br>
-       Ik zit graag achter de computer, al is het om te gamen, youtube of netflix te kijken. Ik heb nog maar weinig ervaring met programmeren, de eerste keer dat ik ermee bezig geweest ben was voor de intake van deze opleiding.</p>
+  <div id="idBody" class="wrapper">
+
+    <div class="grid">
+      <div class="img1"> <img src="../assets/IMG/portretrick.jpeg" alt=""> </div>
+
+      <div class="text-line1"></div>
+      <div class="text-box1">
+        <h1>Wie ben ik</h1><br>
+        <p>Mijn naam is Rick Wiersma, ik ben 17 jaar en woon in Metslawier.<br>
+          Mijn hobby's zijn gamen, racen, uitgaan en DJ'en. <br> Vanaf dat ik 12 was ben ik al bezig met computers, ik heb mijn eigen dan ook zelf gebouwd.  </p>
+      </div>
+
+      <div class="text-box2">
+        <h1>Mijn sterke..</h1><br>
+        <p>Ik ben creatief wat goed van pas komt in het designen van de pagina's/site. Ook kan ik redelijk goed photoshoppen en kan de nodige logo's voor het team/ website maken </p>
+      </div>
+      <div class="text-line2"></div>
+      <div class="text-box2-1">
+        <h1>en minder sterke punten</h1><br>
+        <p>Ik wil graag overal op meekijken en de controle hebben. Ik vertrouw andere mensen niet met mijn werk om het zo te zeggen. Om met meerdere mensen te werken aan een opdracht gebeurt vaak en ik probeer hier ook aan te wennen.</p>
+      </div>
+
+      <div class="text-box3">
+        <h1>Projecten en opdrachten waar ik aan heb gewerkt</h1><br>
+        <p><br><br>Ik heb nog weinig ervaring met programmeren en dus zijn de teamwebsite en de portfolio mijn eerste ervaringen met programmeren. </p>
+      </div>
+      <div class="text-line3"></div>
+      <div class="text-box3-1">
+        <p>
+          <!-- TEKST -->
+        </p>
+      </div>
+
+
+
+
+      <footer>
+        <div class="nested-footer">
+          <p>© HSUNITY</p>
+          <div class="icon1"><a href="https://www.instagram.com/rickwiersma_/" target="_blank"><img src="../assets/IMG/instagram.png" alt="instagram"></a></div>
+          <div class="icon2"><a href="https://www.github.com/HSUNITY" target="_blank"><img src="../assets/IMG/github.png" alt="youtube"></a></div>
+
+
+
+        </div>
+      </footer>
+
+
     </div>
-
-<!--content 2-->
-    <div class="text-box2">
-      <h1>Mijn sterke..</h1><br>
-    <p>Ik ben creatief wat goed van pas komt in het designen van de pagina's/site. Ook kan ik redelijk goed photoshoppen en kan de nodige logo's voor het team/ website maken </p> </div>
-    <div class="text-line2"></div>
-    <div class="text-box2-1">
-    <h1>en minder sterke punten</h1><br>
-    <p>Ik wil graag overal op meekijken en de controle hebben. Ik vertrouw andere mensen niet met mijn werk om het zo te zeggen. Om met meerdere mensen te werken aan een opdracht gebeurt vaak en ik probeer hier ook aan te wennen.</p> </div>
-
-<!--content 3-->
-    <div class="text-box3">
-     <h1>Projecten en opdrachten waar ik aan heb gewerkt</h1><br>
-    <p><br><br>Ik heb nog weinig ervaring met programmeren en dus zijn de teamwebsite en de portfolio mijn eerste ervaringen met programmeren. </p></div>
-    <div class="text-line3"></div>
-    <div class="text-box3-1">
-    <p><!-- TEKST --></p></div>
-
-
-
-
-  <footer>
-  <div class="nested-footer">
-  <p>© HSUNITY</p>
-  <div class="icon1"><a href="https://www.instagram.com/rickwiersma_/" target="_blank"><img src="../assets/IMG/instagram.png" alt="instagram"></a></div>
-  <div class="icon2"><a href="https://www.github.com/HSUNITY" target="_blank"><img src="../assets/IMG/github.png" alt="youtube"></a></div>
-
-
-
-  </div>
-  </footer>
-
-
-  </div>
   </div>
 
+  <script src="../script/nav.js"></script>
+  <script src="../script/init.js"></script>
+  <script src="../script/3am.js"></script>
+</body>
 
-
-
-    <!--scripts-->
-    <script src="../script/nav.js"></script>
-    <script src="../script/init.js"></script>
-    <script src="../script/3am.js"></script>
-  </body>
 </html>
