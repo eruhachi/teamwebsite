8ee89e12083adf603c91c00733fde48cddf4d986
Rowan <rmnorder@gmail.com>
2019-11-01 12:02:39 +0100

    eigen pagioa afgemaakt

--- template/pages/rowan.php
+++ template/pages/rowan.php
@@ -30,31 +30,44 @@
        <div class="grid">
    <!--content 1-->
     <!--replace the # with the name of your image-->
-    <div class="img1"> <img src="../assets/IMG/#" alt=""> </div>
+    <div class="img1"> <img src="../assets/IMG/rowan.jpg" alt=""> </div>
 
     <div class="text-line1"></div>
     <div class="text-box1">
       <h1>Wie ben ik</h1><br>
-    <p><!--put your text here--></p>
+    <p>Mijn naam is Rowan Norder
+				Ik ben 16 jaar en oud,
+				en ik doe de opleiding apllicatie en media-ontwikkelaar. <br>
+        Ik kijk graag naar voetbal en speel geregeld games. <br>
+        Ik speel zelf ook Formule 1 met een racestuur.
+
+
+    </p>
     </div>
-    <div class="deco1"><img src="../assets/IMG/deco1.gif" alt=""> </div>
-    <div class="deco3"><img src="../assets/IMG/deco3.png" alt=""> </div>
+    <div class="deco1"><img src="../assets/IMG/Playstation.png" alt=""> </div>
+    <div class="deco3"><img src="../assets/IMG/Controller.png" alt=""> </div>
 
 <!--content 2-->
     <div class="text-box2">
       <h1>Mijn sterke..</h1><br>
-    <p><!--put your text here--></p> </div>
+    <p>Ik ben erg gezellig, <br>
+        Ik heb mijn werk altijd af, <br>
+        en ik ben er altijd.
+
+    </p> </div>
     <div class="text-line2"></div>
     <div class="text-box2-1">
     <h1>en minder sterke punten</h1><br>
-    <p><!--put your text here--></p> </div>
-    <div class="deco2"> <img src="../assets/IMG/deco2.png" alt="nintendo"></div>
+    <p>Ik ben vaak bij de buren omdat ik daar een paar personen ken waar ik het ook goed mee kan vinden, <br>
+      Ik kan ook wel eens dromerig zijn en dan neem ik even pauze en dan kan ik er weer tegenaan.
+    </p> </div>
+    <div class="deco2"> <img src="../assets/IMG/Controller.png" alt="nintendo"></div>
 
 <!--content 3-->
     <div class="text-box3">
       <h1>Projecten en opdrachten waar ik aan heb gewerkt</h1><br>
-    <p><!--put your text here--></p></div>
-    <div class="deco4"><img src="../assets/IMG/deco1.gif" alt="bow"></div><div class="text-line3"></div><div class="deco5"><img src="../assets/IMG/deco1.gif" alt="bow"></div>
+    <p>Ik heb nu gewerkt aan verschillende websites voor mezelf of voor school</p></div>
+    <div class="deco4"><img src="../assets/IMG/Controller.png" alt="bow"></div><div class="text-line3"></div><div class="deco5"><img src="../assets/IMG/Controller.png" alt="bow"></div>
     <div class="text-box3-1">
     <p><!--put your text here--></p></div>
 
@@ -64,8 +77,8 @@
   <footer>
   <div class="nested-footer">
   <p>© eruhachi</p>
-  <div class="icon1"><a href="https://www.instagram.com/eruhachi/" target="_blank"><img src="../assets/IMG/instagram.png" alt="instagram"></a></div>
-  <div class="icon2"><a href="https://www.youtube.com/user/BenIkJouOma" target="_blank"><img src="../assets/IMG/youtube.png" alt="youtube"></a></div>
+  <div class="icon1"><a href="https://www.instagram.com/rowan_mark_15/" target="_blank"><img src="../assets/IMG/instagram.png" alt="instagram"></a></div>
+  <div class="icon2"><a href="https://www.youtube.com/user/RowanMarkNorder" target="_blank"><img src="../assets/IMG/youtube.png" alt="youtube"></a></div>
 
 
 
